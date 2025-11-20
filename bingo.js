fdd751476ede003e466bda95d0269a44d893d303
Daniel <dangarflo12@gmail.com>
2025-11-20 12:38:18 +0100

    Toca dividir mapas

--- bingo.js
+++ bingo.js
@@ -1,19 +1,18 @@
 let numero=[];
-
 function addNumeroBingo(){
 let valor =Math.floor( Math.random()*90+1);
 numero.push(valor);
 console.log(numero)
+document.getElementById("numeroMostrado").innerHTML=valor;
+
 }
-
-
-
-const bolas1=new Map([]);
-
-
+const bolas=new Map([]);
 function addDiv(){
     let div=document.getElementById("tablero1");
-numero.forEach(n => {
-    
-});
+for(let i=0; i<=numero.length;i++){
+bolas.set(i,numero[i]);
+console.log(bolas);
+console
 }
+}
+
