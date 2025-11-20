e8df4bd50142a588066141b5a6994b2d526420b5
Daniel <dangarflo12@gmail.com>
2025-11-20 11:05:52 +0100

    Añadido mapa bolas1 y se debe de continuar con funcion addDiv

--- bingo.js
+++ bingo.js
@@ -5,3 +5,15 @@ let valor =Math.floor( Math.random()*90+1);
 numero.push(valor);
 console.log(numero)
 }
+
+
+
+const bolas1=new Map([]);
+
+
+function addDiv(){
+    let div=document.getElementById("tablero1");
+numero.forEach(n => {
+    
+});
+}
