2fbeb9e31af9c5028b6c25ea366f557c0a2f8b5c
JosevicSup <josevicss23@gmail.com>
2025-11-24 13:56:40 +0100

    Add files via upload

--- bingo.js
+++ bingo.js
@@ -1,38 +1,4 @@
-/*
-let numero = [];
 
-function addNumeroBingo() {
-    let valor = Math.floor(Math.random() * 90 + 1);
-    
-    while (numero.includes(valor)) {
-        valor = Math.floor(Math.random() * 90 + 1);
-    }
-    
-    numero.push(valor);
-    console.log(numero);
-    
-
-    document.getElementById("numeroMostrado").innerText = valor;
-}
-
-/* LA LOGICA NECESARIA PARA EL BINGO: SE DIVIDE EN DOS MAPAS CON 45 NUMEROS CADA UNO, SE AGREGA LOS NUMEROS(VALORES) POR SU POSICION
-POR EJEMPLO, EL PRIMER NUMERO ES 24, SU POSICION ES 0, POR LO TANTO ES EL PRIMERO QUE SE AGREGA AL HTML, DENTRO DEL TABLERO 1(IZQUIERDO) EN UN CIRCULO QUE CONTIENE EL NUMERO
-CUANDO HAY 45 CIRCULOS, EN OTRAS PALABRAS CUANDO UN TABLERO SE LLENA, SE USA EL SEGUNDO MAPA QUE CONTIENE OTROS 45 NUMEROS*/
-
-/*
-
-const bolas1=new Map([]);
-const bolas2=new Map([]);
-
-
-function addDiv(){
-    let div=document.getElementById("tablero1");
-numero.forEach(n => {
-    
-});
-}
-
-*/
 let numeros = [];
 const bolas1 = new Map();
 const bolas2 = new Map();
@@ -129,7 +95,40 @@ function comprobarBingo() {
 
     alert("¡Bingo válido!");
 }
+function comprobarLinea() {
+    const texto = document.getElementById("lineaComprobar").value.trim();
 
+    if (texto === "") {
+        alert("Introduce números separados por comas");
+        return;
+    }
+
+    // obtener números del input
+    const entradas = texto.split(",");
+    const numsInput = [];
+
+    for (let p of entradas) {
+        const n = Number(p.trim());
+        if (isNaN(n) || n < 1 || n > 90) {
+            alert("Formato incorrecto: solo números entre 1 y 90 separados por comas");
+            return;
+        }
+        numsInput.push(n);
+    }
+
+    // Lista de todos los números que han salido
+    const todos = [...bolas1.values(), ...bolas2.values()];
+
+    // Comprobar uno por uno
+    for (let n of numsInput) {
+        if (!todos.includes(n)) {
+            alert("Número no encontrado: " + n);
+            return;
+        }
+    }
+
+    alert("¡linea válida!");
+}
 
 // Reiniciar todo
 function reiniciar() {
@@ -137,6 +136,8 @@ function reiniciar() {
     bolas1.clear();
     bolas2.clear();
     document.getElementById("numeroMostrado").innerText = "";
+    document.getElementById("lineaComprobar").value= "";
+    document.getElementById("bingoComprobar").value= "";
     crearTableros();
 }
 
@@ -144,4 +145,5 @@ function reiniciar() {
 // Eventos
 document.getElementById("botonGenerar").addEventListener("click", addNumeroBingo);
 document.getElementById("botonComprobar").addEventListener("click", comprobarBingo);
+document.getElementById("botonComprobarlinea").addEventListener("click", comprobarLinea);
 document.getElementById("botonReiniciarBingo").addEventListener("click", reiniciar);
